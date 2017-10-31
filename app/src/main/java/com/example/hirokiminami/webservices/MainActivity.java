281bc6ef889fa65a8621a122ce48576a97d30152
Hiroki Minami <rafael.hiroki@gmail.com>
2017-10-31 18:46:41 -0200

    Faz o post com o edittext

--- app/src/main/java/com/example/hirokiminami/webservices/MainActivity.java
+++ app/src/main/java/com/example/hirokiminami/webservices/MainActivity.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -22,9 +19,7 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 class Participant {
 
@@ -41,22 +36,16 @@ class Participant {
 }
 
 public class MainActivity extends Activity {
-    private List<Participant> participant = new ArrayList<Participant>();
-    private List<HashMap<String, String>> mParticipantsMapList = new ArrayList<>();
-    private static final String KEY_NAME = "name";
-    private static final String KEY_EMAIL = "email";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         final TextView mTextView = (TextView) findViewById(R.id.respTV);
         final Button getReq = (Button) findViewById(R.id.requestBtn);
         final Button postReq = (Button) findViewById(R.id.postRequestBtn);
         final EditText nameET = (EditText) findViewById(R.id.nameET);
         final EditText emailET = (EditText) findViewById(R.id.emailET);
-        final ListView mListView = (ListView) findViewById(R.id.list);
 
         // Instantiate the RequestQueue.
         final RequestQueue queue = Volley.newRequestQueue(this);
@@ -91,11 +80,6 @@ public class MainActivity extends Activity {
                 params.put("name", nameET.getText().toString());
                 params.put("email", emailET.getText().toString());
 
-                ListAdapter adapter = new SimpleAdapter(MainActivity.this, mParticipantsMapList, R.layout.participant,
-                        new String[] { KEY_NAME, KEY_EMAIL },
-                        new int[] { R.id.participantNameTV, R.id.participantEmailTV});
-                mListView.setAdapter(adapter);
-
                 final JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, URL,
                         new JSONObject(params),
                         new Response.Listener<JSONObject>() {
@@ -104,16 +88,6 @@ public class MainActivity extends Activity {
                                 try {
                                     VolleyLog.v("Response:%n %s", response.toString(4));
                                     mTextView.setText("Response is: "+ response.toString());
-                                    for (Participant participant : mParticipantsMapList) {
-
-                                        HashMap<String, String> map = new HashMap<>();
-
-                                        map.put(KEY_VER, android.getVer());
-                                        map.put(KEY_NAME, android.getName());
-                                        map.put(KEY_API, android.getApi());
-
-                                        mAndroidMapList.add(map);
-                                    }
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
